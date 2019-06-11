39d34064f4de14e78c45167dbbc9be40fd794998
msrasheed <moizr95@gmail.com>
2019-06-11 11:16:37 -0500

    Employee Database stuff

---
+++ systemorders-webapp/src/main/java/ws/synopsis/systemorder/model/Employee.java
@@ -0,0 +1,85 @@
+package ws.synopsis.systemorder.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Employees
+ *
+ */
+@Entity
+@Table(name = "employee")
+public class Employee implements Serializable {
+
+	@Id
+	private int userid;
+	private String role;
+	private String fname;
+	private String lname;
+	private String username;
+	private String password;
+	private String email;
+	
+	//private static final long serialVersionUID = 1L;
+
+	public Employee() {
+		super();
+	}
+
+	public int getUserid() {
+		return userid;
+	}
+
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+   
+}
---
+++ systemorders-webapp/src/main/java/ws/synopsis/systemorder/utils/EmployeeDB.java
@@ -0,0 +1,86 @@
+package ws.synopsis.systemorder.utils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import ws.synopsis.systemorder.model.Employee;
+
+public class EmployeeDB {
+	public static Employee getEmployeeByID(int id) {
+		EntityManager em = PostgresDBUtil.getEmFactory().createEntityManager();
+		String qString ="SELECT username" +
+						"FROM Employee e" +
+						"WHERE e.userid = :id";
+		TypedQuery<Employee> q = em.createQuery(qString, Employee.class);
+		q.setParameter("userid", id);
+		try {
+			return q.getSingleResult();
+		} finally {
+			em.close();
+		}
+	}
+	
+	public static String getUsernameByID(int id) {
+		EntityManager em = PostgresDBUtil.getEmFactory().createEntityManager();
+		String qString ="SELECT username" +
+						"FROM Employee e" +
+						"WHERE e.userid = :id";
+		TypedQuery<Employee> q = em.createQuery(qString, Employee.class);
+		q.setParameter("userid", id);
+		try {
+			return q.getSingleResult().getUsername();
+		} finally {
+			em.close();
+		}
+	}
+	
+	public static String getPasswordByUsername(String user) {
+		EntityManager em = PostgresDBUtil.getEmFactory().createEntityManager();
+		String qString =	"SELECT password" +
+							"FROM Employee as e" +
+							"WHERE e.username = user";
+		TypedQuery<Employee> q = em.createQuery(qString, Employee.class);
+		q.setParameter("username", user);
+		try {
+			return q.getSingleResult().getPassword();
+		} finally {
+			em.close();
+		}
+	}
+	
+	public static boolean checkUsernameExists(String user) {
+		EntityManager em = PostgresDBUtil.getEmFactory().createEntityManager();
+		String qString =	"SELECT username" +
+							"FROM Employee as e" +
+							"WHERE e.username = user";
+		TypedQuery<Employee> q = em.createQuery(qString, Employee.class);
+		q.setParameter("username", user);
+		try {
+			if(q.getSingleResult() != null) return true;
+			return false;
+		} finally {
+			em.close();
+		}
+	}
+	
+	public static boolean checkPasswordMatches(String user, String pass) {
+		EntityManager em = PostgresDBUtil.getEmFactory().createEntityManager();
+		String qString =	"SELECT username" +
+							"FROM Employee as e" +
+							"WHERE e.username = user";
+		TypedQuery<Employee> q = em.createQuery(qString, Employee.class);
+		q.setParameter("username", user);
+		try {
+			if(q.getSingleResult().getPassword() == pass) return true;
+			return false;
+		} finally {
+			em.close();
+		}
+	}
+	
+	public static boolean checkCredentials(String user, String pass) {
+		if(checkUsernameExists(user) == true) {
+			if(checkPasswordMatches(user, pass) == true) return true;
+		}
+		return false;
+	}
+}
