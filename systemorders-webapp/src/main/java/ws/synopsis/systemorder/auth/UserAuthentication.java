93df773ff2578904d7106207a0528997fb3b3077
msrasheed <moizr95@gmail.com>
2019-06-14 12:26:45 -0500

    real user authentication

--- systemorders-webapp/src/main/java/ws/synopsis/systemorder/auth/UserAuthentication.java
+++ systemorders-webapp/src/main/java/ws/synopsis/systemorder/auth/UserAuthentication.java
@@ -1,11 +1,16 @@
 package ws.synopsis.systemorder.auth;
 
+import ws.synopsis.systemorder.utils.EmployeeDB;
+import ws.synopsis.systemorder.utils.HashingUtil;
+
 public class UserAuthentication {
 	public static boolean authenticateUser(String username, String password) {
 		boolean isCorrect = false;
 		
-		if (username != null && password != null && 
-				!username.equals("") && !password.equals("")) {
+		password = HashingUtil.shaw256(password);
+		String actualPassword = EmployeeDB.getPasswordByUsername(username);
+		
+		if (password.equals(actualPassword)) {
 			isCorrect = true;
 		}
 		
