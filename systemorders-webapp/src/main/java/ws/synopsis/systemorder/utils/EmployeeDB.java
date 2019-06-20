1371bc4b206ebfe805551dc07e697041c8bb5546
msrasheed <moizr95@gmail.com>
2019-06-20 09:37:27 -0500

    rewinded it - not sure why it was changed anyway

--- systemorders-webapp/src/main/java/ws/synopsis/systemorder/utils/EmployeeDB.java
+++ systemorders-webapp/src/main/java/ws/synopsis/systemorder/utils/EmployeeDB.java
@@ -1,59 +1,40 @@
 package ws.synopsis.systemorder.utils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.persistence.TypedQuery;
 import ws.synopsis.systemorder.model.Employee;
 
 public class EmployeeDB {
-	public static Employee getEmployeeByID(int id) {
+	public static boolean checkCredentials(String username, String password) {
+		if(checkUsernameExists(username) == true) {
+			if(checkPasswordMatches(username, password) == true) return true;
+		}
+		return false;
+	}
+	
+	public static boolean checkPasswordMatches(String username, String password) {
 		EntityManager em = PostgresDBUtil.getEmFactory().createEntityManager();
-		String qString ="SELECT username " +
-						"FROM Employee e " +
-						"WHERE e.userid = :id";
+		String qString =	"SELECT e " +
+							"FROM Employee as e " +
+							"WHERE e.username = :user";
 		TypedQuery<Employee> q = em.createQuery(qString, Employee.class);
-		q.setParameter("userid", id);
+		q.setParameter("user", username);
 		try {
-			return q.getSingleResult();
+			if(q.getSingleResult().getPassword().equals(password)) return true;
+			return false;
 		} finally {
 			em.close();
 		}
 	}
 	
-	public static String getUsernameByID(int id) {
+	public static boolean checkUsernameExists(String username) {
 		EntityManager em = PostgresDBUtil.getEmFactory().createEntityManager();
-		String qString ="SELECT username " +
-						"FROM Employee e " +
-						"WHERE e.userid = :id";
-		TypedQuery<Employee> q = em.createQuery(qString, Employee.class);
-		q.setParameter("userid", id);
-		try {
-			return q.getSingleResult().getUsername();
-		} finally {
-			em.close();
-		}
-	}
-	
-	public static String getPasswordByUsername(String user) {
-		EntityManager em = PostgresDBUtil.getEmFactory().createEntityManager();
-		String qString =	"SELECT password" +
-							"FROM Employee as e" +
-							"WHERE e.username = user";
-		TypedQuery<Employee> q = em.createQuery(qString, Employee.class);
-		q.setParameter("username", user);
-		try {
-			return q.getSingleResult().getPassword();
-		} finally {
-			em.close();
-		}
-	}
-	
-	public static boolean checkUsernameExists(String user) {
-		EntityManager em = PostgresDBUtil.getEmFactory().createEntityManager();
-		String qString =	"SELECT username" +
-							"FROM Employee as e" +
-							"WHERE e.username = user";
-		TypedQuery<Employee> q = em.createQuery(qString, Employee.class);
-		q.setParameter("username", user);
+		String qString =	"SELECT e.username " +
+							"FROM Employee as e " +
+							"WHERE e.username = :user";
+		TypedQuery<String> q = em.createQuery(qString, String.class);
+		q.setParameter("user", username);
 		try {
 			if(q.getSingleResult() != null) return true;
 			return false;
@@ -62,25 +43,104 @@ public class EmployeeDB {
 		}
 	}
 	
-	public static boolean checkPasswordMatches(String user, String pass) {
+	public static Employee getEmployeeByID(int id) {
 		EntityManager em = PostgresDBUtil.getEmFactory().createEntityManager();
-		String qString =	"SELECT username" +
-							"FROM Employee as e" +
-							"WHERE e.username = user";
+		String qString ="SELECT e " +
+						"FROM Employee e " +
+						"WHERE e.userid = :id";
 		TypedQuery<Employee> q = em.createQuery(qString, Employee.class);
-		q.setParameter("username", user);
+		q.setParameter("id", id);
 		try {
-			if(q.getSingleResult().getPassword() == pass) return true;
-			return false;
+			return q.getSingleResult();
 		} finally {
 			em.close();
 		}
 	}
 	
-	public static boolean checkCredentials(String user, String pass) {
-		if(checkUsernameExists(user) == true) {
-			if(checkPasswordMatches(user, pass) == true) return true;
+	public static Employee getEmployeeByUsername(String username) {
+		EntityManager em = PostgresDBUtil.getEmFactory().createEntityManager();
+		String qString ="SELECT e " +
+						"FROM Employee e " +
+						"WHERE e.username = :username";
+		TypedQuery<Employee> q = em.createQuery(qString, Employee.class);
+		q.setParameter("username", username);
+		try {
+			return q.getSingleResult();
+		} catch (Exception e) {
+			return null;
+		}finally {
+			em.close();
 		}
-		return false;
+	}
+	
+	public static String getPasswordByUsername(String username) {
+		EntityManager em = PostgresDBUtil.getEmFactory().createEntityManager();
+		String qString =	"SELECT e.password " +
+							"FROM Employee as e " +
+							"WHERE e.username = :user";
+		TypedQuery<String> q = em.createQuery(qString, String.class);
+		q.setParameter("user", username);
+		try {
+			return q.getSingleResult();
+		} catch (Exception e) {
+			// TODO: handle exception
+			return "";
+		}finally {
+			em.close();
+		}
+	}
+	
+	public static String getUsernameByID(int id) {
+		EntityManager em = PostgresDBUtil.getEmFactory().createEntityManager();
+		String qString ="SELECT e.username " +
+						"FROM Employee e " +
+						"WHERE e.userid = :id";
+		TypedQuery<String> q = em.createQuery(qString, String.class);
+		q.setParameter("id", id);
+		try {
+			return q.getSingleResult();
+		} finally {
+			em.close();
+		}
+	}
+	
+	public static boolean insertEmployee(Employee employee) {
+		boolean isSuccessful = false;
+		
+		EntityManager em = PostgresDBUtil.getEmFactory().createEntityManager();
+		EntityTransaction trans = em.getTransaction();
+		try {
+			trans.begin();
+			em.persist(employee);
+			trans.commit();
+			isSuccessful = true;
+		} catch (Exception e) {
+			trans.rollback();
+			isSuccessful = false;
+		}finally {
+			em.close();
+		}
+		
+		return isSuccessful;
+	}
+	
+	public static boolean mergeEmployee(Employee employee) {
+		boolean isSuccessful = false;
+
+		EntityManager em = PostgresDBUtil.getEmFactory().createEntityManager();
+		EntityTransaction trans = em.getTransaction();
+		try {
+			trans.begin();
+			em.merge(employee);
+			trans.commit();
+			isSuccessful = true;
+		} catch (Exception e) {
+			trans.rollback();
+			isSuccessful = false;
+		}finally {
+			em.close();
+		}
+		
+		return isSuccessful;
 	}
 }
