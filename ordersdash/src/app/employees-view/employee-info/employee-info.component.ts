829b3048046baddd705da5a01eab49b306f326e9
msrasheed <moizr95@gmail.com>
2019-06-27 12:54:20 -0500

    user forms created - ugly right now - no AJAX

--- ordersdash/src/app/employees-view/employee-info/employee-info.component.ts
+++ ordersdash/src/app/employees-view/employee-info/employee-info.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
 import { EmployeesRESTfulService } from '../employees-restful.service';
 import { Employee } from '../employee';
 
@@ -9,16 +10,31 @@ import { Employee } from '../employee';
   styleUrls: ['./employee-info.component.css']
 })
 export class EmployeeInfoComponent implements OnInit {
+
   private emp: Employee;
+  @ViewChild('f') form: any;
+  private showChangePass: boolean = false;
 
   constructor(private route: ActivatedRoute, private emphttp: EmployeesRESTfulService) {;
     this.route.params.subscribe(params => {
       this.emp = this.emphttp.getEmployeeFromId(params['id']);
-      console.log(this.emp);
+      this.showChangePass = false;
+      //console.log(this.emp);
     });
   }
 
   ngOnInit() {
   }
 
+  updateEmployee() {
+    console.log("no update code yet");
+  }
+
+  showPasswordDiag() {
+    this.showChangePass = !this.showChangePass;
+  }
+
+  changePassword() {
+    console.log("no change password code yet");
+  }
 }
--- ordersdash/src/app/employees-view/employees-view.module.ts
+++ ordersdash/src/app/employees-view/employees-view.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 import { UsersTrayComponent } from './users-tray/users-tray.component';
 import { EmployeesModuleViewComponent } from './employees-module-view/employees-module-view.component';
 import { EmployeeTrayItemComponent } from './employee-tray-item/employee-tray-item.component';
@@ -15,6 +16,7 @@ import { EmployeeInfoComponent } from './employee-info/employee-info.component';
   imports: [
     CommonModule,
     HttpModule,
+    FormsModule,
     routing
   ],
   declarations: [UsersTrayComponent, EmployeesModuleViewComponent, EmployeeTrayItemComponent, EmployeeListComponent, GenericComponent, EmployeeInfoComponent],
